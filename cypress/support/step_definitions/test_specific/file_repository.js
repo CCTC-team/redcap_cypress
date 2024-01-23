98d3dd300f4bc4ee64568ef65d2ee28b646dac56
Mintoo Xavier <min2xavier@gmail.com>
2024-01-23 11:36:44 +0000

    Modified step definition for File Repository

--- cypress/support/step_definitions/test_specific/file_repository.js
+++ cypress/support/step_definitions/test_specific/file_repository.js
@@ -98,17 +98,34 @@ Given("I click on the download icon(s) to receive the file(s) for the {string} f
 
 defineParameterType({
     name: 'optionlabel',
-    regexp: /(Restore deleted file\?|Permanently delete file\? \(admins only\))/
+    regexp: /(Restore deleted file|Permanently delete file)/
 })
 
 /**
  * @module FileRepository
  * @author Mintoo Xavier <min2xavier@gmail.com>
- * @example I click on the image {optionlabel} for the file {string} in File Repository
+ * @example I click on the {optionlabel} image for the file {string} in File Repository
  * @param {optionlabel} option - option to click for the file in File Repository
  * @param {string} filename - name of the file for which the option is clicked
  * @description Clicks on a the given option for the file in File Repository
  */
-Given('I click on the image {optionlabel} for the file {string} in File Repository', (option, filename) => {
-    cy.get('td').contains(filename).parents('tr').find('td:nth-child(7)').find('a[title="' + option + '"]').click()
+Given('I click on the {optionlabel} image for the file {string} in File Repository', (option, filename) => {
+    cy.get('td').contains(filename).parents('tr').find('td:nth-child(7)').find('a[title*="' + option + '"]').click()
+})
+
+
+/**
+ * @module FileRepository
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file, and clicking the button labeled {string} to upload the file
+ * @param {string} format - the format of the file that is being uploaded (e.g. csv)
+ * @param {string} file_location - the location of the file being uploaded (e.g. import_files/core/filename.csv)
+ * @param {string} uplaod_label - text near the upload label
+ * @param {string} button_label - text on the button you click to upload
+ * @description Imports well-formed REDCap data import file (of specific type) to a specific project given a Project ID.
+ */
+Given("I upload a {string} format file located at {string}, by clicking the button near {string} in File Repository to browse for the file, and clicking the button labeled {string} to upload the file", (format, file_location, upload_text, button_label) => {
+    cy.upload_file(file_location, format, '', button_label, upload_text).then(() => {
+        cy.get(':button:visible').contains(new RegExp("^" + button_label + "$", "g")).click()
+    })
 })
