e538d1b86dcc59b5e0ac61738f2e8d3f0846fc00
Mintoo Xavier <min2xavier@gmail.com>
2024-09-20 12:10:34 +0100

    Added new step definition for e-consent Framework

--- cypress/support/step_definitions/mintoo.js
+++ cypress/support/step_definitions/mintoo.js
@@ -164,3 +164,22 @@ Given("I should NOT see the Delete image for the event named {string}", (label) 
         expect(tr).to.not.have.descendants('img[title*=Delete]')
     }
 )})  
+
+/**
+ * @module e-consent
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I select the radio option {string} for the e-consent Framework
+ * @param {string} option - option to click for the e-consent Framework
+ * @description Clicks on the given option for the e-consent Framework
+ */
+Given('I select the radio option {string} for the e-consent Framework', (option) => {
+    let value = 0;
+
+    if (option == 'Auto-Archiver enabled')
+        value = 1
+    else if (option == 'Auto-Archiver + e-Consent Framework')
+        value = 2
+    else
+        value = 0;
+    cy.get('input[type=radio][name=pdf_auto_archive][value='+ value + ']').click()
+})
