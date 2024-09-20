13c9d14a4745a131efeeade9dbae6721fd3f3bca
Mintoo Xavier <min2xavier@gmail.com>
2024-09-20 16:24:58 +0100

    Added new step definition for e-consent

--- cypress/support/step_definitions/mintoo.js
+++ cypress/support/step_definitions/mintoo.js
@@ -165,6 +165,7 @@ Given("I should NOT see the Delete image for the event named {string}", (label) 
     }
 )})  
 
+
 /**
  * @module e-consent
  * @author Mintoo Xavier <min2xavier@gmail.com>
@@ -182,4 +183,88 @@ Given('I select the radio option {string} for the e-consent Framework', (option)
     else
         value = 0;
     cy.get('input[type=radio][name=pdf_auto_archive][value='+ value + ']').click()
+})
+
+
+/**
+ * @module Download
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should NOT see the following values in the downloaded PDF for Record {string} and Survey {string}
+ * @param {string} record - the ID of the record the PDF is associated with
+ * @param {string} survey - the Survey / Event of the record the PDF is associated with
+ * @description Verifies the values within a PDF in the PDF Archive
+ */
+Given("I should NOT see the following values in the downloaded PDF for Record {string} and Survey {string}", (record, survey, dataTable) => {
+    //Make sure DataTables has loaded before we do anything here
+    cy.wait_for_datatables().assertWindowProperties()
+
+    //Make sure the page is not loading
+    if(Cypress.$('#file-repository-table_processing:visible').length){
+        cy.get('#file-repository-table_processing').should('have.css', 'display', 'none')
+    }
+
+    const base_element = `${window.tableMappings['file repository']}:visible tr:has(td:nth-child(3):has(a:contains(${JSON.stringify(record)}))):has(:contains(${JSON.stringify(survey)}))`
+    const element_selector = `td i.fa-file-pdf`
+    let pdf_file = null
+
+    function findDateFormat(str) {
+        for (const format in window.dateFormats) {
+            const regex = window.dateFormats[format]
+            const match = str.includes(format)
+            if (match) {
+                expect(window.dateFormats).to.haveOwnProperty(format)
+                return str.replace(format, '')
+            }
+        }
+        return null
+    }
+
+    function waitForFile(filename, timeout = 30000) {
+        const startTime = Date.now()
+
+        const checkFile = (resolve, reject) => {
+            cy.fileExists(pdf_file).then((file) => {
+                if (file === undefined) {
+                    cy.wait(500).then(() => checkFile(resolve, reject))
+                } else if(file){
+                    resolve(file)
+                } else if (Date.now() - startTime > timeout) {
+                    reject(new Error('File not found within timeout period'))
+                } else {
+                    cy.wait(500).then(() => checkFile(resolve, reject))
+                }
+            })
+        }
+
+        return new Cypress.Promise((resolve, reject) => {
+            checkFile(resolve, reject)
+        })
+    }
+
+    cy.top_layer(element_selector, base_element).within(() => {
+        cy.get('td:has(i.fa-file-pdf) a').then(($a) => {
+
+            pdf_file = `cypress/downloads/${$a.text()}`
+
+            waitForFile(pdf_file).then((fileExists) => {
+                if(fileExists){
+                    cy.task('readPdf', { pdf_file: pdf_file }).then((pdf) => {
+                        dataTable['rawTable'].forEach((row, row_index) => {
+                            row.forEach((dataTableCell) => {
+                                const result = findDateFormat(dataTableCell)
+                                if (result === null) {
+                                    expect(pdf.text).to.include(dataTableCell)
+                                } else {
+                                    result.split(' ').forEach((item) => {
+                                        expect(pdf.text).to.not.include(item)
+                                    })
+                                }
+                            })
+                        })
+                    })
+                }
+            })
+        })
+    })
+
 })
