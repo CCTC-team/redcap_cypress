e20d7b2dec61a76856b27a58344d1cff76544e9c
MintooXavier <min2xavier@gmail.com>
2024-10-25 14:59:32 +0100

    Added step definitions for PDF download

--- cypress/support/step_definitions/mintoo.js
+++ cypress/support/step_definitions/mintoo.js
@@ -28,7 +28,7 @@ Given('I select the radio option {string} for the e-consent Framework', (option)
  * @example I should NOT see the following values in the downloaded PDF for Record {string} and Survey {string}
  * @param {string} record - the ID of the record the PDF is associated with
  * @param {string} survey - the Survey / Event of the record the PDF is associated with
- * @description Verifies the values within a PDF in the PDF Archive
+ * @description Verifies the values are not present within a PDF in the PDF Archive
  */
 Given("I should NOT see the following values in the downloaded PDF for Record {string} and Survey {string}", (record, survey, dataTable) => {
     //Make sure DataTables has loaded before we do anything here
@@ -89,7 +89,7 @@ Given("I should NOT see the following values in the downloaded PDF for Record {s
                             row.forEach((dataTableCell) => {
                                 const result = findDateFormat(dataTableCell)
                                 if (result === null) {
-                                    expect(pdf.text).to.include(dataTableCell)
+                                    expect(pdf.text).to.not.include(dataTableCell)
                                 } else {
                                     result.split(' ').forEach((item) => {
                                         expect(pdf.text).to.not.include(item)
@@ -103,4 +103,143 @@ Given("I should NOT see the following values in the downloaded PDF for Record {s
         })
     })
 
+})
+
+
+/**
+ * @module Downloadß
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should see the following values in the downloaded PDF
+ * @param {string} pdf_file - Name od PDF file
+ * @description Verifies the values within a PDF
+ */
+Given("I should see the following values in the downloaded PDF", (dataTable) => {
+    cy.task('fetchLatestDownload', ({fileExtension: 'pdf'})).then((pdf_file) => {
+        // if(latest !== "the latest") 
+        //     pdf_file = "cypress/downloads/" + filename + ".pdf"
+
+    
+
+        function findDateFormat(str) {
+            for (const format in window.dateFormats) {
+                const regex = window.dateFormats[format]
+                const match = str.includes(format)
+                if (match) {
+                    expect(window.dateFormats).to.haveOwnProperty(format)
+                    return str.replace(format, '')
+                }
+            }
+            return null
+        }
+
+        function waitForFile(filename, timeout = 30000) {
+            const startTime = Date.now()
+
+            const checkFile = (resolve, reject) => {
+                cy.fileExists(pdf_file).then((file) => {
+                    if (file === undefined) {
+                        cy.wait(500).then(() => checkFile(resolve, reject))
+                    } else if(file){
+                        resolve(file)
+                    } else if (Date.now() - startTime > timeout) {
+                        reject(new Error('File not found within timeout period'))
+                    } else {
+                        cy.wait(500).then(() => checkFile(resolve, reject))
+                    }
+                })
+            }
+
+            return new Cypress.Promise((resolve, reject) => {
+                checkFile(resolve, reject)
+            })
+        }
+
+        waitForFile(pdf_file).then((fileExists) => {
+            if(fileExists){
+                cy.task('readPdf', { pdf_file: pdf_file }).then((pdf) => {
+                    dataTable['rawTable'].forEach((row, row_index) => {
+                        row.forEach((dataTableCell) => {
+                            const result = findDateFormat(dataTableCell)
+                            if (result === null) {
+                                expect(pdf.text).to.include(dataTableCell)
+                            } else {
+                                result.split(' ').forEach((item) => {
+                                    expect(pdf.text).to.include(item)
+                                })
+                            }
+                        })
+                    })
+                })
+            }
+        })
+    })
+})
+
+/**
+ * @module Downloadß
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should NOT see the following values in the downloaded PDF
+ * @param {string} pdf_file - Name od PDF file
+ * @description Verifies the values are not present within a PDF
+ */
+Given("I should NOT see the following values in the downloaded PDF", (dataTable) => {
+    cy.task('fetchLatestDownload', ({fileExtension: 'pdf'})).then((pdf_file) => {
+        // if(latest !== "the latest") 
+        //     pdf_file = "cypress/downloads/" + filename + ".pdf"
+
+    
+
+        function findDateFormat(str) {
+            for (const format in window.dateFormats) {
+                const regex = window.dateFormats[format]
+                const match = str.includes(format)
+                if (match) {
+                    expect(window.dateFormats).to.haveOwnProperty(format)
+                    return str.replace(format, '')
+                }
+            }
+            return null
+        }
+
+        function waitForFile(filename, timeout = 30000) {
+            const startTime = Date.now()
+
+            const checkFile = (resolve, reject) => {
+                cy.fileExists(pdf_file).then((file) => {
+                    if (file === undefined) {
+                        cy.wait(500).then(() => checkFile(resolve, reject))
+                    } else if(file){
+                        resolve(file)
+                    } else if (Date.now() - startTime > timeout) {
+                        reject(new Error('File not found within timeout period'))
+                    } else {
+                        cy.wait(500).then(() => checkFile(resolve, reject))
+                    }
+                })
+            }
+
+            return new Cypress.Promise((resolve, reject) => {
+                checkFile(resolve, reject)
+            })
+        }
+
+        waitForFile(pdf_file).then((fileExists) => {
+            if(fileExists){
+                cy.task('readPdf', { pdf_file: pdf_file }).then((pdf) => {
+                    dataTable['rawTable'].forEach((row, row_index) => {
+                        row.forEach((dataTableCell) => {
+                            const result = findDateFormat(dataTableCell)
+                            if (result === null) {
+                                expect(pdf.text).to.not.include(dataTableCell)
+                            } else {
+                                result.split(' ').forEach((item) => {
+                                    expect(pdf.text).to.not.include(item)
+                                })
+                            }
+                        })
+                    })
+                })
+            }
+        })
+    })
 })
