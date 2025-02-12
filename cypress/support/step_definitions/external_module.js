319e0f5fb2d49d5f6a57c5037d6bb48234e79d04
MintooXavier <min2xavier@gmail.com>
2025-02-12 12:39:39 +0000

    Added SD for EM

--- cypress/support/step_definitions/external_module.js
+++ cypress/support/step_definitions/external_module.js
@@ -8,61 +8,20 @@ defineParameterType({
 })
 
   /**
-   * @module ExternalModule
+   * @module ControlCenter
    * @author Mintoo Xavier <min2xavier@gmail.com>
    * @example I {externalOption} the external module named {string}
    * @param {string} externalOption - available options - 'Enable', 'Delete Version'
    * @param {string} label - name of external module
    * @description Enable/Disable external module
    */
-  Given("I {externalOption} the external module named {string}", (option, label) => {
+  Given("I click on the button labeled {externalOption} for the external module named {string}", (option, label) => {
     cy.get('#external-modules-disabled-table').find('td').contains(label).parents('tr').within(() => {
         cy.get('button').contains(option).click()
     })
   })
 
-
-  /**
-   * @module Visibility
-   * @author Mintoo Xavier <min2xavier@gmail.com>
-   * @example I should see the field labeled {string} disabled
-   * @param {string} field_name - name of field
-   * @description Verify field is disabled
-   */
-  Given("I should see the field labeled {string} disabled", (field_name) => {
-    let contains = ''
-    let last_label = field_name
-    field_name.split(' ').forEach((val) => {
-        contains += `:has(:contains(${JSON.stringify(val)}))`
-        last_label = val
-    })
-    let outer_element = `tr${contains}:has(input[type=text]):visible:first`
-
-    cy.get(outer_element).within(() => {
-        cy.get(`label:contains(${JSON.stringify(last_label)})`)
-            .invoke('attr', 'id')
-            .then(($id) => {
-                let elm = cy.get('[name="' + $id.split('label-')[1] + '"]')
-                elm.should('have.attr', 'disabled');
-            })
-    })
-  })
-
-
-  /**
-   * @module ExternalModule
-   * @author Mintoo Xavier <min2xavier@gmail.com>
-   * @example I {externalOption} the external module named {string}
-   * @param {string} externalOption - available options - 'Enable', 'Delete Version'
-   * @param {string} label - name of external module
-   * @description Enable/Disable external module
-   */
-  Given("I should see {string} within the data entry field labeled {string}", (option, label) => {
-    cy.get('#questiontable').find('td').contains(label).parents('tr').within(() => {
-        cy.get('small').should('contain', option)
-    })
-  })
-
+  
 
 /**
  * @module ControlCenter
@@ -99,6 +58,48 @@ Given('I (should )see the {string} request created for the project named {string
 })
 
 
+  /**
+   * @module Visibility
+   * @author Mintoo Xavier <min2xavier@gmail.com>
+   * @example I should see the field labeled {string} disabled
+   * @param {string} field_name - name of field
+   * @description Verify field is disabled
+   */
+  Given("I should see the field labeled {string} disabled", (field_name) => {
+    let contains = ''
+    let last_label = field_name
+    field_name.split(' ').forEach((val) => {
+        contains += `:has(:contains(${JSON.stringify(val)}))`
+        last_label = val
+    })
+    let outer_element = `tr${contains}:has(input[type=text]):visible:first`
+
+    cy.get(outer_element).within(() => {
+        cy.get(`label:contains(${JSON.stringify(last_label)})`)
+            .invoke('attr', 'id')
+            .then(($id) => {
+                let elm = cy.get('[name="' + $id.split('label-')[1] + '"]')
+                elm.should('have.attr', 'disabled');
+            })
+    })
+  })
+
+
+  /**e
+   * @module EmbellishFields
+   * @author Mintoo Xavier <min2xavier@gmail.com>
+   * @example I should see {string} within the data entry field labeled {string}
+   * @param {string} fieldOptions - field options visible
+   * @param {string} label - Field Label
+   * @description Enable/Disable external module
+   */
+  Given("I should see {string} within the data entry field labeled {string}", (fieldOptions, label) => {
+    cy.get('#questiontable').find('td').contains(label).parents('tr').within(() => {
+        cy.get('small').should('contain', fieldOptions)
+    })
+  })
+
+
 /**
  * @module HightlightDQR
  * @author Mintoo Xavier <min2xavier@gmail.com>
@@ -127,10 +128,25 @@ Given('I (should )see the field labeled {string} highlighed in red', (label) => 
 /**
  * @module HightlightDQR
  * @author Mintoo Xavier <min2xavier@gmail.com>
- * @example I (should )NOT see the field labeled {string} highlighed in red
+ * @example I should NOT see the field labeled {string} highlighed in red
  * @param {string} label - field label
  * @description verify field is not highlighted in red
  */
-Given('I (should )NOT see the field labeled {string} highlighed in red', (label) => {
+Given('I should NOT see the field labeled {string} highlighed in red', (label) => {
     cy.get('#questiontable').find('tr').contains(label).parents('tr').should('not.have.attr', 'style', 'border-width: 2px; border-color: rgb(255, 33, 0);')
+})
+
+
+/**
+ * @module DataEntryLog
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should see {int} row(s) in the data entry log table
+ * @param {int} num - number of row(s)
+ * @description verifies data entry log table contains the specified number of row(s)
+ */
+Given('I should see {int} row(s) in the data entry log table', (num) => {
+    cy.get('#log-data-entry-event tbody tr').its('length').then ((rowCount) => {
+        // Subtracting 1 for header
+        expect(rowCount-1).to.be.equal(num)
+    })
 })
