98455dfe18695a12fa10cd82cb0a36cb085d2555
MintooXavier <min2xavier@gmail.com>
2025-03-03 17:22:23 +0000

    Added SD for Monitoring QR

--- cypress/support/step_definitions/external_module.js
+++ cypress/support/step_definitions/external_module.js
@@ -243,3 +243,54 @@ Given('I should see {formStatusIcon} bubble with the form status {string}', (ico
 Given('I should NOT see {formStatusIcon} form status bubble', (icon) => {
     cy.get('#questiontable').should('not.contain', formStatusIcon[icon])
 })
+
+
+/**
+ * @module MonitoringQR
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I enter {string} in the column {string} for the field {string}
+ * @param {string} text - text to enter
+ * @param {string} col - column in monitoring table
+ * @param {string} fieldLabel - field label
+ * @description enters text in the input field for the specified row and column in the monitoring table
+ */
+Given('I enter {string} in the column {string} for the field {string}', (text, col, fieldLabel) => {
+    cy.table_cell_by_column_and_row_label(col, fieldLabel, '#mon-q-fields-table').find('input').clear().type(text)
+})
+
+
+/**
+ * @module MonitoringQR
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should NOT see {string} in the monitoring table
+ * @param {string} text - text that should not be seen in the monitoring table
+ * @description verifies text is not visible in the monitoring table
+ */
+Given('I should NOT see {string} in the monitoring table', (text) => {
+    cy.get('#mon-q-fields-table').should('not.contain', text)
+})
+
+
+/**
+ * @module MonitoringQR
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I select {string} in the dropdown field in column {string} for the field {string}
+ * @param {string} option - option to select
+ * @param {string} col - column in monitoring table
+ * @param {string} fieldLabel - field label
+ * @description selects the dropdown option for the specified row and column in the monitoring table
+ */
+Given('I select {string} in the dropdown field in column {string} for the field {string}', (option, col, fieldLabel) => {
+    cy.table_cell_by_column_and_row_label(col, fieldLabel, '#mon-q-fields-table').find('select').select(option)  
+})
+
+
+/**
+ * @module MonitoringQR
+ * @author Mintoo Xavier <min2xavier@gmail.com>
+ * @example I should NOT see the monitoring table
+ * @description verifies monitoring table does not exists
+ */
+Given('I should NOT see the monitoring table', () => {
+    cy.get('#mon-q-fields-table').should('not.exist')
+})
